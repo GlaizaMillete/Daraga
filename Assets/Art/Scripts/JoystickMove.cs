0b95dd6b1c66cf9e48f750e015afdc66fe6dce1e
Kenny <127096433+Kennyyboi@users.noreply.github.com>
2024-08-20 23:57:52 +0800

    joystickMove

--- Assets/Art/Scripts/JoystickMove.cs
+++ Assets/Art/Scripts/JoystickMove.cs
@@ -2,52 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JoystickMove : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    public Joystick movementJoystick;
+    public   
+ Joystick movementJoystick;
     public float playerSpeed;
     private Rigidbody2D rb;
-    private   
- Animator animator;
+    private Animator animator;
     private SpriteRenderer spriteRenderer;
 
-    private float horizontalInput;
     private   
- bool isFacingRight = true;
-
-    private void Start() 
+ void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();   
+
+
+        // ... (rest of your code)
     }
 
-    private void FixedUpdate()   
- 
+    private void FixedUpdate() 
     {
         if (rb != null && movementJoystick != null) 
         {
-            horizontalInput = movementJoystick.Direction.x;
-
+            float moveX = movementJoystick.Direction.x;
+    
             // Set the player's velocity based on the joystick input
-            rb.velocity = new Vector2(horizontalInput * playerSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(moveX * playerSpeed, rb.velocity.y);
 
             // Update the Animator with the movement input
-            animator.SetFloat("xVelocity", Mathf.Abs(horizontalInput));
+            animator.SetFloat("xVelocity", Mathf.Abs(moveX));
 
-            FlipSprite();
-        }
-    }
-
-    private void FlipSprite()
-    {
-        // If the player is moving left and facing right, or moving right and facing left, flip the sprite
-        if (horizontalInput < 0 && isFacingRight || horizontalInput > 0 && !isFacingRight)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 ls = transform.localScale;
-            ls.x *= -1f;
-            transform.localScale = ls;
+            // Flip the sprite based on movement direction
+            if (moveX < 0 && !spriteRenderer.flipX)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (moveX > 0 && spriteRenderer.flipX)
+            {
+                spriteRenderer.flipX = false;
+            }
         }
     }
 }
